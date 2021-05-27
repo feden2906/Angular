b2e9d1ed36449f365309218a9dc5074309d677fa
Vladzo <vashchukvlad17@gmail.com>
2021-05-27 21:51:56 +0300

    Angular homework1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
+import {HttpClientModule} from "@angular/common/http";
+import {UserService} from "./services/user.service";
+import { UserComponent } from './components/user/user.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
+import {PostService} from "./services/post.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    PostsComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UserService, PostService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

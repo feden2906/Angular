f031e96598b0e05f045bcc2e0ede3fea5c875059
Fedenko Maksym <feden2906@gmail.com>
2021-05-30 12:21:44 +0300

    imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { UsersComponent } from './components/users/users.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
-import {UserService} from "./services/user.service";
-import { UserComponent } from './components/user/user.component';
-import { PostsComponent } from './components/posts/posts.component';
-import { PostComponent } from './components/post/post.component';
-import {PostService} from "./services/post.service";
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {PostComponent, PostsComponent, UserComponent, UsersComponent} from './components';
+import {PostService, UserService} from "./services";
 
 @NgModule({
   declarations: [

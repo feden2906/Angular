9255bb128a730740479061be05893dfe0b1f320d
Vladzo <vashchukvlad17@gmail.com>
2021-06-06 15:04:59 +0300

    add index.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {UsersComponent} from './components/users/users.component';
+import {UsersComponent, UserComponent, UserDetailsComponent, PostsComponent,
+  PostComponent, PostDetailsComponent} from './components';
 import {HttpClientModule} from "@angular/common/http";
-import {UserComponent} from './components/user/user.component';
 import {RouterModule, Routes} from "@angular/router";
-import {UserDetailsComponent} from './components/user-details/user-details.component';
-import {PostsComponent} from './components/posts/posts.component';
-import {PostComponent} from './components/post/post.component';
-import {PostDetailsComponent} from './components/post-details/post-details.component';
 import {UsersResolveService} from "./services/resolvers/users.resolve.service";
 import {PostsResolveService} from "./services/resolvers/posts.resolve.service";
 
